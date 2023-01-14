8f78ed7d2c8936ce0cd2179277a421d45d55e57d
borjahz <borja.h.z.11@gmail.com>
2023-01-14 08:48:55 +0100

    terminada la relacion entre tablas

--- app/src/main/java/com/example/applicationfinal/DataHolder.java
+++ app/src/main/java/com/example/applicationfinal/DataHolder.java
@@ -1,5 +1,21 @@
 package com.example.applicationfinal;
 
 public class DataHolder {
+private static DataHolder instance;
+private String data;
+
+private DataHolder(){}
+    public static DataHolder getInstance(){
+    if(instance==null){
+        instance=new DataHolder();
+    }
+    return instance;
+    }
+    public String getData(){
+    return data;
+    }
+    public void setData(String data) {
+        this.data = data;
+    }
 
 }
